e9e9562d0565919c1999f99298d790c036fbb1ef
Edgard FInotti Neto <edgardfinotti@yahoo.com.br>
2023-08-28 15:58:13 -0300

    adicionado styled components no projeto, criado arquivo de temas da aplicacao e a tipagem desses temas.

--- github-blog/src/App.tsx
+++ github-blog/src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from 'styled-components'
 import './App.css'
+import { defaultTheme } from './styles/themes/default'
 
 function App() {
-  return <>GITHUB BLOG</>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <>GITHUB BLOG</>
+    </ThemeProvider>
+  )
 }
 
 export default App
